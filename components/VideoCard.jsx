c9829a27e0559ce1caf6c955f81c611ea98cb9ca
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-09-29 20:17:26 +0530

    adding

---
+++ components/VideoCard.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { ResizeMode, Video } from "expo-av";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+import { icons } from "../constants";
+
+const VideoCard = ({ title, creator, avatar, thumbnail, video,ex }) => {
+  const [play, setPlay] = useState(false);
+
+  return (
+    <View className="flex flex-col items-center px-4 mb-14">
+      <View className="flex flex-row gap-3 items-start">
+        <View className="flex justify-center items-center flex-row flex-1">
+          <View className="w-[46px] h-[46px] rounded-lg border border-secondary flex justify-center items-center p-0.5">
+            <Image
+              source={{ uri: avatar }}
+              className="w-full h-full rounded-lg"
+              resizeMode="cover"
+            />
+          </View>
+
+          <View className="flex justify-center flex-1 ml-3 gap-y-1">
+            <Text
+              className="font-psemibold text-sm text-black"
+              numberOfLines={3}
+            >
+              {title}
+            </Text>
+            <Text
+              className="text-xs text-black font-pregular"
+              numberOfLines={2}
+            >
+              {creator}
+            </Text>
+          </View>
+        </View>
+
+        <View className="pt-2">
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
+        </View>
+      </View>
+
+      {play ? (
+        <Video
+          source={{ uri: video }}
+          className="w-full h-60 rounded-xl mt-3"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
+      ) : (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+          className="w-full h-60 rounded-xl mt-3 relative flex justify-center items-center"
+        >
+          <Image
+            source={{ uri: thumbnail }}
+            className="w-full h-full rounded-xl mt-3"
+            resizeMode="cover"
+          />
+
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+
+      )}
+  <Text className="text-sm text-black text-center mt-7 font-pregular">
+        {ex}
+      </Text>
+
+
+    </View>
+  );
+};
+
+export default VideoCard;
