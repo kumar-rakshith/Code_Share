5b651cfeca092239e66ee99f61ac033de95d8db7
Rakshith Kumar <rakshithkumar.thokur@gmail.com>
2024-10-30 20:44:14 +0530

    Update EmptyState.jsx

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -4,6 +4,7 @@ import { View, Text, Image } from "react-native";
 import { images } from "../constants";
 import CustomButton from "./CustomButton";
 
+
 const EmptyState = ({ title, subtitle }) => {
   return (
     <View className="flex justify-center items-center px-4">
