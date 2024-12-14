34780ad364a54dca2e9a7150fe9e7ef581ef785d
Rakshith Kumar <rakshithkumar.thokur@gmail.com>
2024-12-14 22:10:35 +0530

    Update GlobalProvider.js

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -2,8 +2,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 
 import { getCurrentUser } from "../lib/appwrite";
-
-
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
 
